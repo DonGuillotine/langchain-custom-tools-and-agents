ddef73ec10a873e053844ebd8da34a5a22388221
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-18 05:35:39 +0100

    Feat ✨: Switched the Search Tool to SerpAPI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain import OpenAI 
 from langchain.chat_models import ChatAnthropic
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.tools import DuckDuckGoSearchRun
+from langchain.utilities import SerpAPIWrapper
 from langchain.agents import Tool
 from langchain.tools import BaseTool
 from decouple import config
@@ -9,16 +9,14 @@ from decouple import config
 
 llm = ChatAnthropic(anthropic_api_key=config("ANTHROPIC_API_KEY"), temperature=0)
 
-
-search = DuckDuckGoSearchRun()
-# defining a single tool
+search = SerpAPIWrapper(serpapi_api_key=config("SERPAPI_API_KEY"))
 tools = [
     Tool(
-        name = "search",
+        name = "Current Search",
         func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions"
-    )
+        description="useful for when you need to answer questions about current events or the current state of the world"
+    ),
 ]
 
-obj = search.run("How do I treat an ant sting?")
-print(obj)
+response = search.run("How do I treat an ant sting?")
+print(response)
