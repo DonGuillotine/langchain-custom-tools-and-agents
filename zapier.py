509779e373e1fbee6d00207691e04b4449cee1b0
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-24 02:09:50 +0100

    Feat ✨: Setup LLM, Zapier Toolkit and Agent

--- zapier.py
+++ zapier.py
@@ -3,3 +3,12 @@ from langchain.agents import initialize_agent
 from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.agents import AgentType
 from langchain.utilities.zapier import ZapierNLAWrapper
+from decouple import config
+
+
+llm = OpenAI(openai_api_key=config('OPENAI_API_KEY'), temperature=0)
+zapier = ZapierNLAWrapper(zapier_nla_api_key=config('ZAPIER_NLA_API_KEY'))
+toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+agent = initialize_agent(
+    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
